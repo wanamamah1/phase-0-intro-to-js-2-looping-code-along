f94ce4773dc104020703d54cc6c8b89402f43e2d
aggrey wanamamah <aggreywanamamah008@gmail.com>
2025-04-17 22:32:46 +0100

    creates index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+// Task 1
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+
+//Task 2
+function countDown(number) {
+    if (number < 0) {
+      console.log("Please provide a positive integer.");
+      return;
+    }
+    let counter = number;
+    while (counter >= 0) {
+      console.log(counter);
+      counter--; 
+    }
+  }
